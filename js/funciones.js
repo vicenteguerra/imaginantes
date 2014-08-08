5580d5079ba2fc545d28d4a54836bc4dd5d9222f
Vicente <vicenteguerrahdez@gmail.com>
2014-08-07 22:56:03 -0500

    web

---
+++ js/funciones.js
@@ -0,0 +1,22 @@
+
+
+$(function() {
+var fixadent = $("#menu"), pos = fixadent.offset();
+var main = $("#main");
+$(window).scroll(function() {
+if($(this).scrollTop() > (pos.top + 0) && fixadent.css('position') == 'static') 
+	{ 
+		fixadent.addClass('fixed'); 
+		main.addClass('mainFixed');
+	}
+else if($(this).scrollTop() <= pos.top && fixadent.hasClass('fixed'))
+	{ 
+		fixadent.removeClass('fixed'); 
+		main.removeClass('mainFixed');
+	}
+})
+});
+
+
+ 
+    
